ffdc084314f1b598f3f94c8d5c260dae947a7de6
KStockley <keirans@gmail.com>
2018-09-26 15:55:08 -0700

    Complete helper functions; need to work on solvers

--- src/Board.js
+++ src/Board.js
@@ -62,14 +62,14 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
     \__ \ || (_| | |  | |_  | | | |  __/ | |  __/_
     |___/\__\__,_|_|   \__| |_| |_|\___|_|  \___(_)
 
- */
+    */
     /*=========================================================================
     =                 TODO: fill in these Helper Functions                    =
     =========================================================================*/
@@ -79,12 +79,26 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var present = false;
+      for (let i = 0; i < rowIndex.length; i++) {
+        if (rowIndex[i] && present) {
+          return true;
+        } else if (rowIndex[i]) {
+          present = true;
+        }
+      }
+      return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var board = this.rows();
+      for (var row of board) {
+        if (this.hasRowConflictAt(row)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -94,12 +108,37 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      // generate a board
+      var board = this.rows();
+      // make a Boolean to represent the presence of a piece
+      var present = false;
+      // for each row of the board
+      for (var row of board) {
+        // if the value at colIndex of the row contains a piece AND a piece is already present
+        if (row[colIndex] && present) {
+          // we have a conflict and return true
+          return true;
+        // otherwise if the value at colIndex contains a piece
+        } else if (row[colIndex]) {
+          // then we set Boolean to true
+          present = true;
+        }
+      }
+      // return false if for loop finishes without returning true
+      return false;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      // iterate over each column of the board (n)
+      for (let i = 0; i < this.get('n'); i++) {
+        // invoke hasColConflictAt passing each colIndex, and if that results in true, immediately return true
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+      // if no conflicts are found, return false
+      return false;
     },
 
 
@@ -109,12 +148,38 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      // create a Boolean that represents a piece already found
+      var present = false;
+      // create a board
+      var board = this.rows();
+      // iterate over each row of the board
+      for (let i = 0; i < board.length; i++) {
+        // if the offset column index of this row contains a piece and a piece already has been found
+        if (board[i][i + majorDiagonalColumnIndexAtFirstRow] && present) {
+          // return true
+          return true;
+        // otherwise if this index contains a piece 
+        } else if (board[i][i + majorDiagonalColumnIndexAtFirstRow]) {
+          // set the Boolean to true
+          present = true;
+        }
+      }
+      // if no conflict is found return false
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      // iterate over every offset column index of the first row
+      for (let i = -(this.get('n')); i < this.get('n'); i++) {
+        // if the resulting diagonal has a conflict
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          // immediatly return true
+          return true;
+        }
+      }
+      // otherwise no conflict is found return false
+      return false;
     },
 
 
@@ -124,12 +189,38 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      // create a Boolean that represents a piece already found
+      var present = false;
+      // create a board
+      var board = this.rows();
+      // iterate over each row of the board
+      for (let i = 0; i < board.length; i++) {
+        // if the offset column index of this row contains a piece and a piece already has been found
+        if (board[i][minorDiagonalColumnIndexAtFirstRow - i] && present) {
+          // return true
+          return true;
+        // otherwise if this index contains a piece 
+        } else if (board[i][minorDiagonalColumnIndexAtFirstRow - i]) {
+          // set the Boolean to true
+          present = true;
+        }
+      }
+      // if no conflict is found return false
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      // iterate over every offset column index of the first row
+      for (let i = 0; i < 2 * (this.get('n')); i++) {
+        // if the resulting diagonal has a conflict
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          // immediatly return true
+          return true;
+        }
+      }
+      // otherwise no conflict is found return false
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
