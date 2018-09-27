f54e7560962fb31f6bba576fe99481ef310b6467
KStockley <keirans@gmail.com>
2018-09-26 21:36:17 -0700

    implement helper and solver methods

--- src/solvers.js
+++ src/solvers.js
@@ -16,32 +16,174 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // create a board that has n rows and columns
+  var board = new Board({n: n});
+  // make counter of non-conflicting pieces
+  var nonConflictingPieces = 0;
+  // make an array to store valid solutions
+  var solutions;
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var recursiveFunc = (row = 0, col = 0) => {
+    // iterate through each row
+    for (let i = row; i < n; i++) {
+      // iterate through each column index in a row
+      for (let j = i === row ? col : 0; j < n; j++) {
+        // toggle the piece on the column index of the row
+        board.togglePiece(i, j);
+        // if there is any conflicting piece on rows and columns
+        if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()) {
+          // delete the piece
+          board.togglePiece(i, j);
+        } else {
+          // increment number of valid pieces
+          nonConflictingPieces++;
+          // recurse over possible permutations with the current configuration of the board
+          return recursiveFunc(i, j + 1);
+        }
+      }
+    }
+    
+    // if counter of non-conflicting pieces equal value of n
+    if (nonConflictingPieces === n) {
+      console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board.rows()));
+      return board.rows();
+    }      
+  };
+  return recursiveFunc();
 };
+  
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  
+  // create a board that has n rows and columns
+  var board = new Board({n: n});
+  // make counter of non-conflicting pieces
+  var nonConflictingPieces = 0;
+  // make an array to store valid solutions
+  var solutions = 0;
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  var recursiveFunc = (row = 0, col = 0) => {
+    // iterate through each row
+    for (let i = row; i < n; i++) {
+      // iterate through each column index in a row
+      for (let j = i === row ? col : 0; j < n; j++) {
+        // toggle the piece on the column index of the row
+        board.togglePiece(i, j);
+        // if there is any conflicting piece on rows and columns
+        if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()) {
+          // delete the piece
+          board.togglePiece(i, j);
+        } else {
+          // increment number of valid pieces
+          nonConflictingPieces++;
+          // recurse over possible permutations with the current configuration of the board
+          recursiveFunc(i, j + 1);
+          // undo most recent piece placement once all results of that configuration are checked
+          board.togglePiece(i, j);
+          // decrement number of valid pieces
+          nonConflictingPieces--;
+        }
+      }
+    }
+    
+    // if counter of non-conflicting pieces equal value of n
+    if (nonConflictingPieces === n) { // if n valid pieces are placed
+      solutions++; // increment number of solutions
+    }      
+  };
+
+  recursiveFunc(); // invoke recursive helper function
+
+  console.log('Number of solutions for ' + n + ' rooks:', solutions);
+  return solutions;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  if (n === 2 || n === 3) {
+    return new Board({n: n}).rows();
+  }
+  // create a board that has n rows and columns
+  var board = new Board({n: n});
+  // make counter of non-conflicting pieces
+  var nonConflictingPieces = 0;
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  var recursiveFunc = (row = 0, col = 0) => {
+    // iterate through each row
+    for (let i = row; i < n; i++) {
+      // iterate through each column index in a row
+      for (let j = i === row ? col : 0; j < n; j++) {
+        // toggle the piece on the column index of the row
+        board.togglePiece(i, j);
+        // if there is any conflicting piece on rows and columns
+        if (board.hasAnyRowConflicts() || board.hasAnyColConflicts() || board.hasAnyMajorDiagonalConflicts() || board.hasAnyMinorDiagonalConflicts()) {
+          // delete the piece
+          board.togglePiece(i, j);
+        // otherwise recursive call to a chessboard that starts on 
+        } else {
+          nonConflictingPieces++;
+          if (recursiveFunc(i, j + 1)) {
+            return board.rows().slice(0);
+          }
+          // undo most recent piece placement once all results of that configuration are checked
+          board.togglePiece(i, j);
+          // decrement number of valid pieces
+          nonConflictingPieces--;
+        }
+      }
+    }
+    
+    // if counter of non-conflicting pieces equal value of n
+    if (nonConflictingPieces === n) {
+      console.log('Single solution for ' + n + ' queens:', JSON.stringify(board.rows()));
+      return board.rows();
+    }      
+  };
+  return recursiveFunc();
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  // create a board that has n rows and columns
+  var board = new Board({n: n});
+  // make counter of non-conflicting pieces
+  var nonConflictingPieces = 0;
+  // make an array to store valid solutions
+  var solutions = 0;
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  var recursiveFunc = (row = 0, col = 0) => {
+    // iterate through each row
+    for (let i = row; i < n; i++) {
+      // iterate through each column index in a row
+      for (let j = i === row ? col : 0; j < n; j++) {
+        // toggle the piece on the column index of the row
+        board.togglePiece(i, j);
+        // if there is any conflicting piece on rows and columns
+        if (board.hasAnyRowConflicts() || board.hasAnyColConflicts() || board.hasAnyMajorDiagonalConflicts() || board.hasAnyMinorDiagonalConflicts()) {
+          // delete the piece
+          board.togglePiece(i, j);
+        } else {
+          // increment number of valid pieces
+          nonConflictingPieces++;
+          // recurse over possible permutations with the current configuration of the board
+          recursiveFunc(i, j + 1);
+          // undo most recent piece placement once all results of that configuration are checked
+          board.togglePiece(i, j);
+          // decrement number of valid pieces
+          nonConflictingPieces--;
+        }
+      }
+    }
+    
+    // if counter of non-conflicting pieces equal value of n
+    if (nonConflictingPieces === n) { // if n valid pieces are placed
+      solutions++; // increment number of solutions
+    }      
+  };
+
+  recursiveFunc(); // invoke recursive helper function
+
+  console.log('Number of solutions for ' + n + ' queens:', solutions);
+  return solutions;
 };
